52651a3b98012a609292bf9024fb89d7a58e6e52
Kaapo.Studio <contact@kaapo.studio>
2023-09-15 08:53:41 +0300

    Dark mode added to localStorage;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import {Component, HostBinding, OnInit, signal} from '@angular/core';
+import {Component, effect, HostBinding, OnInit, signal} from '@angular/core';
 import {initFlowbite} from "flowbite";
 
 @Component({
   selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  darkMode = signal<boolean>(false)
+  darkMode = signal<boolean>(JSON.parse(window.localStorage.getItem('darkMode') ?? 'false'))
+
+  constructor() {
+    effect(() => {
+      window.localStorage.setItem('darkMode', JSON.stringify(this.darkMode()))
+    })
+  }
 
   @HostBinding('class.dark') get mode() {
     return this.darkMode();
